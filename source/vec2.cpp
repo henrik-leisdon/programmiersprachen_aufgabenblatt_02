5c1d2a686cf0c95f6765926890e0252233dce4c2
thetrippleh <henrik.leisdon@uni-weimar.de>
2018-05-04 14:55:03 +0200

    add task 2.3 (without tests)

--- source/vec2.cpp
+++ source/vec2.cpp
@@ -12,7 +12,39 @@ Vec2::Vec2(float x, float y):
     x{x},
     y{y} {}
 
+Vec2& Vec2::operator+=(Vec2 const& v){
+    x = v.x + x;
+    y = v.y + y;
+    
+    return *this;
+}
 
+Vec2& Vec2::operator-=(Vec2 const& v){
+    x = x - v.x;
+    y = y - v.y;
+
+    return *this;
+}
+
+Ved2& Vec2::operator*=(float v){
+    x = x * v;
+    y = y * v;
+
+    return *this;
+}
+
+Ved2& Vec2::operator/=(float v){
+    if (v == 0.0f)
+    {
+        std::cout << "divsion by 0 not possible"
+    }
+    else
+    {
+        x = x / v;
+        y = y / v;
+    }
+    return *this;
+}
 
 
 
